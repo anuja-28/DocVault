896f65b1f7b81f6296a7d5a6a4861ee0e7712e61
anuja-28 <anuamale19@gmail.com>
2026-07-09 23:15:06 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/acube/docvault/DocvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acube.docvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
